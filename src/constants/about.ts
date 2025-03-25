d2a27a76703f6cc2a3d98c7630ed8f531a0ba10c
do-huni <orangnlp@gmail.com>
2025-03-25 15:06:04 +0900

    feat: about me

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -1,5 +1,3 @@
-import type { Experience } from "~types/experience.type";
-
 export const FEATURED_STACK_IDS = [
   1, // React
   2, // NestJS
@@ -32,3 +30,36 @@ export const FEATURED_STACK_IDS = [
   31, // FastAPI
   32, // Spring
 ];
+
+export const EDUCATIONS = [
+  {
+    school: "고려대학교 서울캠퍼스",
+    majors: ["영어영문", "소프트웨어기술벤처"],
+    degree: "학사",
+    startedAt: "2020.03",
+    endedAt: "2026.02",
+    gpa: {
+      score: 3.8,
+      maxScore: 4.5,
+    },
+    description: `
+영어학 전공자로서 촘스키의 '보편 문법'을 통해 <strong>인간 언어가 사고를 표현하는 추상화의 과정</strong>임을 배웠습니다.
+이러한 '추상화' 개념이 단지 <strong>자연어</strong>에 국한되지 않고 <strong>더 낮은 수준의 언어 체계에도 동일하다는 생각</strong>에 보다 근본적 추상화인 <strong>기계어</strong>를 공부했습니다.
+특히, <strong>소프트웨어</strong>가 이러한 추상화의 정점에서 실제적이고 구체적인 가치를 창출하는 점에 매료돼 전공하게 되었습니다.
+`,
+    achievements: [
+      "데이터통신, 운영체제, 정보보호 등 CS 과목 다수 수강",
+      "소프트웨어 개발 학회 DevKor 운영진",
+      "교내 캡스톤 디자인 프로젝트 참여",
+      "영문학 소모임 LIT 부회장",
+    ],
+  },
+];
+
+export const ABOUT_ME = `
+안녕하세요! 저는 백엔드 개발자 Astor입니다. <strong>소프트웨어 아키텍처</strong>에 깊은 관심을 가지고 있으며, 문제 해결을 위한 최적의 구조화된 소프트웨어를 설계하는 것을 즐깁니다.
+<br />
+스타트업의 <strong>초기 창업 멤버</strong>로서, 제로 베이스에서 견고한 아키텍처와 안정적인 시스템을 구축하여 일일 1000+ 건의 결제 요청을 성공적으로 처리하는 서비스를 개발했습니다.
+<br />
+급변하는 환경에서도 유연한 아키텍처를 통해 효율적인 시스템 운영을 실현하고, <strong>비즈니스 가치와 기술적 완성도를 균형 있게 달성</strong>하는 데 주력하고 있습니다.
+`;
