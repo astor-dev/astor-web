2c34855d7d51cd96a8e49b08bba197849386beee
astor-dev <orangnlp@gmail.com>
2025-09-04 16:00:07 +0900

    feat:aboutme 자격증 추가

--- src/common/constants/about.ts
+++ src/common/constants/about.ts
@@ -1,4 +1,5 @@
 import type { EducationDetail } from "~features/about/components/education-list.astro";
+import type { CertificationDetail } from "~features/about/components/certification-list.astro";
 
 export const FEATURED_STACK_IDS: number[] = [
   1, // React
@@ -50,12 +51,45 @@ export const EDUCATIONS: EducationDetail[] = [
 특히, <strong>소프트웨어</strong>가 이러한 추상화의 정점에서 실제적이고 구체적인 가치를 창출하는 점에 매료돼 전공하게 되었습니다.
 `,
     activitiesAndAchievements: [
-      "자료구조, 운영체제, 데이터베이스, 클라우드 컴퓨팅 등 CS 과목 42학점 수강",
+      "CS 핵심 전공 42학점 이수(운영체제, 네트워크, 소프트웨어공학, 클라우드 컴퓨팅 등)",
       "창업 경진대회 STARTUP EXPRESS 츄츄기업가상 수상(핸디버스)",
       "산학 캡스톤 디자인 프로젝트 우수상 수상",
       "영어영문학과 학생회 국원 및 영문학 소모임 LIT 부회장",
     ],
   },
+];
+
+export const CERTIFICATIONS: CertificationDetail[] = [
+  {
+    name: "정보처리기사",
+    issuer: "한국산업인력공단",
+    issuedAt: "2025.09",
+  },
+  {
+    name: "SW역량테스트 Professional",
+    issuer: "삼성SDS",
+    issuedAt: "2025.08",
+  },
+  // {
+  //   name: "군 장병 AI·SW 역량강화 교육",
+  //   issuer: "정보통신기획평가원",
+  //   type: "completion",
+  //   issuedAt: "2023.08",
+  //   description1:
+  //     "Python 기반 머신러닝 기술을 활용한 인공지능 프로젝트 교육과정 수료",
+  //   description2:
+  //     "데이터 전처리, 모델 훈련, 성능 평가 등 실습을 통해 AI 개발 역량을 습득",
+  // },
+  // {
+  //   name: "인공지능 BM 교육과정",
+  //   issuer: "서울ICT이노베이션스퀘어",
+  //   type: "completion",
+  //   issuedAt: "2022.04",
+  //   description1:
+  //     "2022년 AI 연구 동향 및 RNN, CNN 등 딥러닝 모델의 동작 원리에 대한 이론적 학습",
+  //   description2:
+  //     "AI 기술을 활용한 스타트업 사례 분석 및 비즈니스 모델 기반 사업 계획서 작성",
+  // },
 ];
 
 export const ABOUT_ME = `
